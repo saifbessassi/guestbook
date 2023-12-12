0dc1e01e12054d6b6101fc2c019ff66db781da90
Saif BESSASSI <sa.bessassi@gmail.com>
2023-12-12 15:32:51 +0100

    set createdAt date of comment automatically

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Comment
 {
     #[ORM\Id]
@@ -36,6 +37,11 @@ class Comment
     public function __toString()
     {
         return (string) $this->getEmail();
+    }
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void {
+        $this->createdAt = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
